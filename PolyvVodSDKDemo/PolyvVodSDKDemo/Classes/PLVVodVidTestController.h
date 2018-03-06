4672b55f4ce21e8d7df4d9826303252f3fb06937
bqlin <linbinquan@polyv.net>
2018-03-06 14:14:44 +0800

    添加指定 vid 测试页面

---
+++ PolyvVodSDKDemo/PolyvVodSDKDemo/Classes/PLVVodVidTestController.h
@@ -0,0 +1,17 @@
+//
+//  PLVVodVidTestController.h
+//  PolyvVodSDKDemo
+//
+//  Created by Bq Lin on 2018/3/5.
+//  Copyright © 2018年 POLYV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class PLVVodVideo;
+
+@interface PLVVodVidTestController : UIViewController
+
+/// 可选传入 PLVVodVideo 对象
+@property (nonatomic, strong) PLVVodVideo *video;
+
+@end
---
+++ PolyvVodSDKDemo/PolyvVodSDKDemo/Classes/PLVVodVidTestController.m
@@ -0,0 +1,78 @@
+//
+//  PLVVodVidTestController.m
+//  PolyvVodSDKDemo
+//
+//  Created by Bq Lin on 2018/3/5.
+//  Copyright © 2018年 POLYV. All rights reserved.
+//
+
+#import "PLVVodVidTestController.h"
+#import "PLVVodSkinPlayerController.h"
+
+@interface PLVVodVidTestController ()<UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UIView *playerPlaceholder;
+
+@property (weak, nonatomic) IBOutlet UITextView *videoInfoTextView;
+
+/// 带皮肤播放器
+@property (nonatomic, strong) PLVVodSkinPlayerController *player;
+
+@end
+
+@implementation PLVVodVidTestController
+
+- (void)dealloc {
+	NSLog(@"%s", __FUNCTION__);
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+	
+	PLVVodSkinPlayerController *player = [[PLVVodSkinPlayerController alloc] initWithNibName:nil bundle:nil];
+	[player addPlayerOnPlaceholderView:self.playerPlaceholder rootViewController:self];
+	self.player = player;
+}
+
+- (BOOL)prefersStatusBarHidden {
+	return self.player.prefersStatusBarHidden;
+}
+- (UIStatusBarStyle)preferredStatusBarStyle {
+	return self.player.preferredStatusBarStyle;
+}
+
+#pragma property
+
+- (void)setVideo:(PLVVodVideo *)video {
+	_video = video;
+	dispatch_async(dispatch_get_main_queue(), ^{
+		self.player.video = video;
+		self.videoInfoTextView.text = video.description;
+	});
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+	NSString *vid = textField.text;
+	vid = [vid stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	__weak typeof(self) weakSelf = self;
+	[PLVVodVideo requestVideoWithVid:vid completion:^(PLVVodVideo *video, NSError *error) {
+		weakSelf.video = video;
+	}];
+	
+	return NO;
+}
+
+#pragma mark - action
+
+- (IBAction)doneAction:(UIBarButtonItem *)sender {
+	[self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)testAction:(UIBarButtonItem *)sender {
+	NSLog(@"%s", __FUNCTION__);
+}
+
+@end
