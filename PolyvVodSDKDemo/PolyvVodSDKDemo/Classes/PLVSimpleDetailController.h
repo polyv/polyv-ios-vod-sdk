9ded248cded9c170c2a9b1a231fb05031f6dcb12
lien <libenlong@corp.polyv.net>
2018-06-08 10:47:53 +0800

    dd

---
+++ PolyvVodSDKDemo/PolyvVodSDKDemo/Classes/PLVSimpleDetailController.h
@@ -0,0 +1,15 @@
+//
+//  PLVSimpleDetailController.h
+//  PolyvVodSDKDemo
+//
+//  Created by Bq Lin on 2018/3/26.
+//  Copyright © 2018年 POLYV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLVSimpleDetailController : UIViewController
+
+@property (nonatomic, copy) NSString *vid;
+
+@end
---
+++ PolyvVodSDKDemo/PolyvVodSDKDemo/Classes/PLVSimpleDetailController.m
@@ -0,0 +1,55 @@
+//
+//  PLVSimpleDetailController.m
+//  PolyvVodSDKDemo
+//
+//  Created by Bq Lin on 2018/3/26.
+//  Copyright © 2018年 POLYV. All rights reserved.
+//
+
+#import "PLVSimpleDetailController.h"
+#import <PLVVodSDK/PLVVodSDK.h>
+#import "PLVVodSkinPlayerController.h"
+
+@interface PLVSimpleDetailController ()
+
+@property (weak, nonatomic) IBOutlet UIView *playerPlaceholder;
+@property (nonatomic, strong) PLVVodSkinPlayerController *player;
+
+@end
+
+@implementation PLVSimpleDetailController
+
+- (void)dealloc {
+	//NSLog(@"%s", __FUNCTION__);
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	
+	[self setupPlayer];
+}
+
+- (void)setupPlayer {
+	// 初始化播放器
+	PLVVodSkinPlayerController *player = [[PLVVodSkinPlayerController alloc] initWithNibName:nil bundle:nil];
+	[player addPlayerOnPlaceholderView:self.playerPlaceholder rootViewController:self];
+	self.player = player;
+	NSString *vid = self.vid;
+	__weak typeof(self) weakSelf = self;
+	[PLVVodVideo requestVideoWithVid:vid completion:^(PLVVodVideo *video, NSError *error) {
+		if (!video.available) return;
+		weakSelf.player.video = video;
+		dispatch_async(dispatch_get_main_queue(), ^{
+			weakSelf.title = video.title;
+		});
+	}];
+}
+
+- (BOOL)prefersStatusBarHidden {
+	return self.player.prefersStatusBarHidden;
+}
+- (UIStatusBarStyle)preferredStatusBarStyle {
+	return self.player.preferredStatusBarStyle;
+}
+
+@end
