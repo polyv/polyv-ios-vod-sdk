56725806176e041b1784f0dfedd2fb98bd1b644c
lien <libenlong@corp.polyv.net>
2018-07-17 18:24:00 +0800

    - 点播SDK hls视频增加EXT-X-DISCONTINUITY支持 - hls 视频，播放时前后台切换，返回前台时视频界面卡住问题修复 - 视频总时长显示优化，取videoJson中媒体时长，取代播放器自身媒体时长 - 播放离线视频失败问题修复 - 32 位机器获取时间戳兼容性问题修复 - 自定义播放器时间显示，添加使用注释

---
+++ PolyvVodSDKDemo/PolyvVodSDKDemo/PolyvOpenSourceModule/Category/UIButton+EnlargeTouchArea.h
@@ -0,0 +1,15 @@
+//
+//  UIButton+EnlargeTouchArea.h
+//  PolyvVodSDKDemo
+//
+//  Created by mac on 2018/7/6.
+//  Copyright © 2018年 POLYV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (EnlargeTouchArea)
+
+- (void) setEnlargeEdgeWithTop:(CGFloat) top right:(CGFloat) right bottom:(CGFloat) bottom left:(CGFloat) left;
+
+@end
